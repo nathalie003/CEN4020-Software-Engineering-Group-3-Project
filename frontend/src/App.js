73607ae225963e26bc835523422ccd366bab79f9
aeduff <aea.duffey@gmail.com>
2025-02-09 17:06:25 -0500

    Co-authored-by: terryalmeida <terryalmeida@users.noreply.github.com> Co-authored-by: GiannaMeisner <GiannaMeisner@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">hello
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+// export default function App() {
+//   return (
+//     <section>
+//       <h1>Spiced Chai Recipe</h1>
+//       <h2>For two</h2>
+//       <Recipe drinkers={2} />
+//       <h2>For a gathering</h2>
+//       <Recipe drinkers={4} />
+//     </section>
+//   );
+// }
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../Assets/Logo.png'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='LeftSide'>
+        <img src={Logo}/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
