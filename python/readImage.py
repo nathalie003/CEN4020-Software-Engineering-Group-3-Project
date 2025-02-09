1dbc9df75c80c373dea1463398ed86646e152222
aeduff <aea.duffey@gmail.com>
2025-02-09 14:05:52 -0500

    testing

---
+++ python/readImage.py
@@ -0,0 +1,30 @@
+from PIL import Image 
+from pytesseract import pytesseract 
+from pdf2image import convert_from_path
+import os
+
+image_path = r"C:\SWE-Project\EERIS-3v2\CEN4020-Software-Engineering-Group-3-Project\python\Receipts\Orders & Purchases _ Costco.pdf"
+if not os.path.exists(image_path):
+    print("File not found:", image_path)
+else:
+    print("File exists!")
+images = convert_from_path(image_path)
+  
+# Defining paths to tesseract.exe 
+# and the image we would be using 
+path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+  
+# Opening the image & storing it in an image object 
+img = images[0]
+  
+# Providing the tesseract executable 
+# location to pytesseract library 
+pytesseract.tesseract_cmd = path_to_tesseract 
+  
+# Passing the image object to image_to_string() function 
+# This function will extract the text from the image 
+text = pytesseract.image_to_string(img) 
+  
+# Displaying the extracted text 
+print(text[:-1])
