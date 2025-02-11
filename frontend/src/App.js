eb9856047bec02f4ffeed305cbe5dbbb8a666859
aeduff <aea.duffey@gmail.com>
2025-02-10 21:06:00 -0500

    up to date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">hello
+    <div className="App">
       <Router>
         <Navbar />
         <Routes>
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import Logo from '../Assets/Logo.png'
+import React from 'react';
+import Logo from '../Assets/Logo.png';
+import {Link} from 'react-router-dom';
+import '../Styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className='navbar'>
       <div className='LeftSide'>
         <img src={Logo}/>
+      </div>
+      <div className='RightSide'>
+        <Link to='/'>Home</Link>
+        <Link to='/Menu'>Menu</Link>
+        <Link to='/About'>About</Link>
+        <Link to='/Contact'>Contact</Link>
       </div>
     </div>
   )
