f1bc86aa0d5d8d102a6b5962204ff94da9f24ca6
terry <terryalmeida787@gmail.com>
2025-02-13 12:55:48 -0500

    small changes

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,6 @@ const app = express();//needed for mysql database
 app.use(cors());//needed for mysql database
 app.use(express.json());
 
-
-// const db = mysql.createConnection({
-//     host: 'mysql-database-1-cashpilotproject.crua2cacctz3.us-east-1.rds.amazonaws.com',
-//     port: '3306',
-//     user: 'cashpilotadmin',
-//     password: 'shZBhkTS4lGd',
-//     database: 'user'
-
-// })//needed for mysql database
-// //mysql-database-1-cashpilotproject.crua2cacctz3.us-east-1.rds.amazonaws.com
-
-
 // Test route to verify backend connectivity
 app.get('/', (req, res) => {
     res.json("From Backend Side");
@@ -39,27 +27,4 @@ app.get('/user', (req, res) => {
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
-// //needed for mysql database
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: 'root',
-//     password: '',
-//     database: 'test'
-
-// })
-
-// app.post("/upload", upload.single("file"), async (req, res) => {
-//     if (!req.file) return res.status(400).json({ error: "No file uploaded" });
-
-//     try {
-//         // Send file to Python OCR microservice
-//         const response = await axios.post("http://localhost:5001/extract_text", req.file.buffer, {
-//             headers: { "Content-Type": "image/png" } // Adjust for file type
-//         });
-
-//         res.json({ text: response.data.text });
-//     } catch (error) {
-//         res.status(500).json({ error: "OCR service error" });
-//     }
-// });
 
