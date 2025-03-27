2af0fe4c11e198bdfbd92dd3e04d5f9ad36d4e1c
Terry Almeida <terryalmeida787@gmail.com>
2025-03-26 23:48:40 -0400

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React, {useState} from 'react';
 //import './App.css';
 import HomeLanding from './Pages/HomePage/HomeLanding';
 import logo from './Components/Images/CashPilot.png';
-const port = process.env.PORT || 4000;
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
