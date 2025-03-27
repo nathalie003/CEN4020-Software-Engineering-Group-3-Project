7bd18cfaceff8bf0780ebc92c35d114fcee3f6d9
Terry Almeida <terryalmeida787@gmail.com>
2025-03-26 22:13:28 -0400

    fix home names

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import CreateAccountLanding from './Pages/Home Page/CreateAccountLanding.js';
-import LogInLanding from './Pages/Home Page/LogInLanding.js';
-import AdminLanding from './Pages/Admin Page/AdminLanding';
-import EmployeeLanding from './Pages/Employee Page/EmployeeLanding';
-import SupervisorLanding from './Pages/Supervisor Page/SupervisorLanding';
-import AutoFilledForm from './Pages/Employee Page/AutoFilledForm';
-import ManualEntryForm from './Pages/Employee Page/ManualEntryForm';
+import CreateAccountLanding from './Pages/HomePage/CreateAccountLanding.js';
+import LogInLanding from './Pages/HomePage/LogInLanding.js';
+import AdminLanding from './Pages/AdminPage/AdminLanding';
+import EmployeeLanding from './Pages/EmployeePage/EmployeeLanding';
+import SupervisorLanding from './Pages/SupervisorPage/SupervisorLanding';
+import AutoFilledForm from './Pages/EmployeePage/AutoFilledForm';
+import ManualEntryForm from './Pages/EmployeePage/ManualEntryForm';
 
 function Main() {
     return (
