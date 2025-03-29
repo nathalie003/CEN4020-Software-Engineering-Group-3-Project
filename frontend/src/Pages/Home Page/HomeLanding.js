3f9affae06fd14e582c4b3888849b319f42b19d8
nathalie003 <nathalied276@gmail.com>
2025-03-29 01:45:32 -0400

    Home page edits

--- frontend/src/Pages/Home Page/HomeLanding.js
+++ frontend/src/Pages/Home Page/HomeLanding.js
@@ -147,15 +147,10 @@ function HomeLanding() {
                 <div className="row">
                     <h2 className="h2 text-center col-12 py-5 semi-bold-600">Services</h2>
                     <div className="service-header text-end light-300">
-                        <div className="icon-text-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="60" fill="currentColor" className="bi bi-laptop fa-pull-left" viewBox="0 0 16 16">
-                                <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5"/>
-                            </svg>
-                        
-                            <div className="text-start pb-3">
-                                <h2 className="h3 pb-3 col-lg-9">Explore Our Comprehensive Services</h2>
-                            </div>
+                        <div className="text-start pb-3">
+                            <h2 className="h3 pb-3 col-lg-9">Explore Our Comprehensive Services</h2>
                         </div>
+
                     </div>
                 </div>
                 <p className="service-footer col-10 offset-2 col-lg-9 offset-lg-3 text-start pb-3 text-muted px-2">
