bff4129d2660cb1a8a9763e2f3d8d1899a03d91c
terry <terryalmeida787@gmail.com>
2025-04-16 15:05:54 -0400

    time

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api', receiptRoutes);
 
 // Root endpoint for basic server check
 app.get("/", (req, res) => {
-  res.json("Backend Running Perfectly 4-16-25 1:13PM");
+  res.json("Backend Running Perfectly 4-16-25 3:05PM");
 });
 
 // Start the server after ensuring a DB connection
